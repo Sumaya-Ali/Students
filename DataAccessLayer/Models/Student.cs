a6cd511b79cb1662103075f6113fdf5f9df32c5b
SumayaAli <Sumaya.Jalal.Ali@gmail.com>
2023-09-12 19:43:30 +0300

    Adding refrences & DB context & Models & Migrations

---
+++ DataAccessLayer/Models/Student.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Models
+{
+    public class Student
+    {
+        [Key]
+        public int Id { get; set; }
+        
+        [Required]
+        public string FirstName { get; set; }
+        
+        [Required]
+        public string LastName { get; set; }
+
+        [EmailAddress(ErrorMessage ="Invalid Email Address")]
+        public string Email { get; set; }
+        
+        [Phone(ErrorMessage ="Invalid Telephon Number")]
+        public string Telephon { get; set; }
+
+        public DateTime BirthDay { get; set; }
+    }
+}
